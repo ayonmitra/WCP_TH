0bfe7d9a20552c57d932df1febe28d4fd91aa637
alexanderfleig <alex@fleigs.com>
2015-02-02 16:01:24 -0500

    Added basic functionality to Entity class, all future characters and items should extend it and use its protected variables.

--- src/alex/classes/Entity.java
+++ src/alex/classes/Entity.java
@@ -1,4 +1,50 @@
 package alex.classes;
 
-public class Entity {
+public class Entity extends Object{
+	
+	protected double hp;
+	protected double attack;
+	protected double defence;
+	protected double speed;
+	protected String name;
+	
+	public double getHp() {
+		return hp;
+	}
+	
+	public void setHp(double newVal) {
+		hp = newVal;
+	}
+	
+	public double getAttack() {
+		return attack;
+	}
+	
+	public void setAttack(double newVal) {
+		attack = newVal;
+	}
+	
+	public double getDefence() {
+		return defence;
+	}
+	
+	public void setDefence(double newVal) {
+		defence = newVal;
+	}
+	
+	public double getSpeed() {
+		return speed;
+	}
+	
+	public void setSpeed(double newVal) {
+		speed = newVal;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String newVal) {
+		name = newVal;
+	}
 }
