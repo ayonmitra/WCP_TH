fdde1438e73174d96314a01bad3aa311bf6023f2
Ayon2 <Ayon2@10.35.16.26>
2015-02-02 16:34:44 -0500

    wekj qjerk jw

--- src/ayon/TestClass.java
+++
@@ -1,9 +0,0 @@
-/*True plot: serf who works on a biscuit farm  and goes to meet the king, but the evil
- * monster doesn't want him to meet the king and sends people to beat him.
- */
-
-package ayon;
-
-public class TestClass {
-
-}
