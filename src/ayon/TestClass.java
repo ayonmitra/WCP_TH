8f865e762ba49c3fd7e3e039fb4a3c0ff5dc6272
Ayon2 <Ayon2@10.35.16.26>
2015-02-02 15:45:48 -0500

    Made my own package and said the real plot

---
+++ src/ayon/TestClass.java
@@ -0,0 +1,9 @@
+/*True plot: serf who works on a biscuit farm  and goes to meet the king, but the evil
+ * monster doesn't want him to meet the king and sends people to beat him.
+ */
+
+package ayon;
+
+public class TestClass {
+
+}
