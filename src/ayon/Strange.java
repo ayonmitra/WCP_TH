dc60eae353b3ea2950e9534dfd0589e1c8985086
Ayon2 <Ayon2@10.35.22.250>
2015-02-03 10:54:43 -0500

    sdjfiaj sdif

---
+++ src/ayon/Strange.java
@@ -0,0 +1,13 @@
+package ayon;
+
+import java.awt.*;
+import java.applet.*;
+
+public class Strange extends Applet{
+
+	private static final long serialVersionUID = -1716079133976794075L;
+
+	public void paint(Graphics g) {
+		g.drawLine(25, 25, 75, 75);
+	}
+}
